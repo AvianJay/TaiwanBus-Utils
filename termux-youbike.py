f2e5f75ba770dfff0c38fac2243d7e318164cae9
AvianJay <jay20090728@gmail.com>
2024-11-15 17:47:54 +0800

    update

--- termux-youbike.py
+++ termux-youbike.py
@@ -2,6 +2,7 @@
 # Require Termux:API
 # not done yet.
 # 針對空位檢測。
+import os
 import sys
 import time
 import json
@@ -45,23 +46,43 @@ def gentext(station):
         return "臥槽你真幸運。"
 
 def get_location(provider="gps"):
+    providers = ["gps", "network", "passive"]
+    if not provider in providers:
+        raise Exception("Wrong Provider")
+    echo("Trying to get location with provider", provider)
     process = subprocess.Popen(["termux-location", "-p", provider], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = process.communicate()
+    providers.remove(provider)
     
     if process.returncode == 0:
-        return json.loads(out)
+        try:
+            return json.loads(out)
+        except ValueError:
+            for p in providers:
+                echo("Failed to get location. Retrying with provider", p, "...", level="WARN")
+                process = subprocess.Popen(["termux-location", "-p", p], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                out, err = process.communicate()
+                try:
+                    return json.loads(out)
+                except ValueError:
+                    pass
+            return None
     else:
         raise Exception(f"Error getting location: {err.decode('utf-8')}")
 
 def checknear(lat, lon, distance, provider="gps"):
-    echo("Start checking device's location...")
+    echo("Getting device's location...")
     location = get_location(provider)
-    echo("Got location")
-    nd = measure(lat, lon, location["latitude"], location["longitude"])
-    echo("Distance:", nd)
-    if nd <= distance:
-        return True
+    if location:
+        echo("Got location")
+        nd = measure(lat, lon, location["latitude"], location["longitude"])
+        echo("Distance:", nd)
+        if nd <= distance:
+            return True
+        else:
+            return False
     else:
+        echo("Failed to get location. Retry next time.")
         return False
 
 def checkuntilnear(lat, lon, distance, provider="gps", checktime=180):
@@ -76,11 +97,19 @@ def main(args):
     if args.cmd == "location":
         echo("Getting data...")
         data = youbike.getstationbyid(args.id)
-        checkuntilnear(data["lat"], data["lng"], args.distance)
+        checkuntilnear(float(data["lat"]), float(data["lng"]), args.distance, args.provider, args.time)
         echo("Getting data...")
         data = youbike.getstationbyid(args.id)
         echo("Sending notify...")
         send_notify(f'[YBP]{data["name_tw"]}', f"你已到達站點附近，剩下 {data['parking_spaces'] - data['available_spaces']} 個空位。{gentext(data)}")
+    elif args.cmd == "time":
+        echo("Not done yet!", level="WARN")
+        while True:
+            echo("Getting data...")
+            data = youbike.getstationbyid(args.id)
+            echo("Sending notify...")
+            send_notify(f'[YBP]{data["name_tw"]}', f"站點剩下 {data['parking_spaces'] - data['available_spaces']} 個空位。{gentext(data)}")
+            time.sleep(args.time)
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description="YouBikePython for Termux")
@@ -88,7 +117,9 @@ if __name__=="__main__":
     parser_location = subparsers.add_parser("location", help="檢測位置模式")
     parser_time = subparsers.add_parser("time", help="持續發送模式")
     parser_location.add_argument("id", help="站點ID", type=str)
-    parser_location.add_argument("-d", "--distance", help="距離（公尺）", type=int, dest="distance", default=500)
+    parser_location.add_argument("-d", "--distance", help="檢測距離（公尺）", type=int, dest="distance", default=500)
+    parser_location.add_argument("-t", "--time", help="檢查間隔時間", type=int, dest="time", default=60)
+    parser_location.add_argument("-p", "--provider", help="位置提供者（gps, network, passive）", type=str, dest="provider", default="gps")
     parser_time.add_argument("id", help="站點ID", type=str)
     parser_time.add_argument("-t", "--time", help="發送間隔時間", type=int, dest="time", default=60)
     args = parser.parse_args()
