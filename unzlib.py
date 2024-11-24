7884023e3de98be3a826d586c6a7b38abb612d23
AvianJay <jay20090728@gmail.com>
2024-11-24 21:09:49 +0800

    better arg and add web

--- unzlib.py
+++ unzlib.py
@@ -3,14 +3,30 @@
 # because the response of yahoo api is zlib
 import sys
 import zlib
+import requests
+import argparse
 
-def decompress(filename, output):
-    open(output, "wb").write(zlib.decompress(open(filename, "rb").read()))
+def decompress(data):
+    return zlib.decompress(data)
 
-if __name__ == "__main__":
-    if not len(sys.argv) == 3:
-        print("Usage:", sys.argv[0], "[Input filename] [Output filename]")
-        sys.exit(1)
-    print("Decompressing", sys.argv[0])
-    decompress(sys.argv[1], sys.argv[2])
-    print("Decompressed file saved to", sys.argv[2])
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description="Unzlib")
+    subparsers = parser.add_subparsers(dest="cmd")
+    parser_web = subparsers.add_parser("web", help="get data with web")
+    parser_file = subparsers.add_parser("file", help="get data with file")
+    parser_web.add_argument("url", help="URL", type=str)
+    parser_web.add_argument("-o", "--output", help="output file", type=str, default=None, dest="out")
+    parser_file.add_argument("file", help="input filename", type=str)
+    parser_file.add_argument("-o", "--output", help="output file", type=str, default=None, dest="out")
+    args = parser.parse_args()
+    if args.cmd=="web":
+        r = requests.get(args.url).content
+        dec = decompress(r)
+    elif args.cmd=="file":
+        d = open(args.file, "r").read()
+        dec = decompress(d)
+    if args.out:
+        open(args.out, "w").write(dec)
+        print("Decompressed data saved to", args.out)
+    else:
+        print(dec)
